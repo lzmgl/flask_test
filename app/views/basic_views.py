f2be6ef1abc7c439e6682b57614633a3c4f6b842
lzmgl5874 <lzmgl5874@gmail.com>
2023-07-12 09:45:00 +0900

    init

---
+++ app/views/basic_views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+from app.models import Question
+from app.models import Answer
+
+# 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
+fisa = Blueprint('basic', __name__, url_prefix='/fisa')
+
+@fisa.route('/post')
+def post_list():
+    question_list = Question.query.all()
+    return render_template('question/question_list.html', question_list=question_list)
+
+@fisa.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
